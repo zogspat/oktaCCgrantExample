67a013323ce88dbbb024e12c6725095e995830d2
Donal Hanna <donal@the-plot.com>
2019-04-09 13:07:43 +0000

    command line client
    - requires Azn server config - creation of custom scope, and access policy for it.
    - requires client application config.
    - sets the access token as an Authorization header.
    - success = return of the json contents of the access token

---
+++ ccGrantClient.py
@@ -0,0 +1,53 @@
+import requests
+import base64
+from requests.auth import HTTPBasicAuth
+import json
+
+
+oktaInstance = "https://dev-533308.okta.com"
+client_id = "yourId"
+client_secret = "yourSecret"
+tokenEndpoint = oktaInstance + "/oauth2/anzSvrIdentifier/v1/token"
+redirUri = "http://localhost:8000"
+scope = "customScope"
+response_type = "code"
+
+testEndpointUrl = "http://ip_of_endpoint:8081/boo"
+
+
+def getAznCode():
+	heads = {'accept': 'application/json',
+			  'cache-control': 'no-cache',
+			  'content-type': 'application/x-www-form-urlencoded'}
+	params = {'grant_type': 'client_credentials',
+				'scope': scope}
+	response = requests.post(tokenEndpoint, auth=HTTPBasicAuth(client_id,client_secret), data=params)
+	if (response.status_code != 200):
+		print("Azn request failed. Error code: ", response.status_code)
+		print("The errors tend to be in the body of the response:", response.content)
+	else:	
+		#print("url: ", response.content)
+		jsonResponse = response.json()
+		try:
+			print("token: ", jsonResponse.get('access_token'))
+			return jsonResponse.get('access_token')
+			# NOTE: there is an expires_in value which has a value set in seconds.
+		except ValueError:
+			return "Catch this. One day"
+
+def makeTestCall(azncode):
+   heads = {'Authorization': azncode,
+   			'accept': 'application/json',
+			'content-type': 'application/x-www-form-urlencoded'}
+   result = requests.get(testEndpointUrl, headers=heads)
+   print("result: ", result.content)
+
+
+
+def main():
+	aznCode = getAznCode()
+	makeTestCall(aznCode)
+
+
+
+if __name__=='__main__': main()
